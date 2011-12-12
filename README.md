a7794462048d47920cd767e5c867d56ae7db749b
Irvin <irvinfly@gmail.com>
2011-12-12 15:45:10 +0800

    Finished 0.5, append plurk2plurk button below every fb like button. visible when hover.
    Signed-off-by: Irvin <irvinfly@gmail.com>

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-This is the P2Plurk add-on.  It contains:
+Firefox Plurk2Plurk extension  v0.5
+Dec. 2011
+Irvin  irvinfly@gmail.com
 
-* A program (lib/main.js).
-* A few tests.
-* Some meager documentation.
+This Add-on will adding 'Share to Plurk' button after every fb Like buttons.
+Plurk is another SNS featuring the 'river style' timeline.
+
+This add-on is licensed under Mozilla Public License Version 1.1
+You may obtain a copy of the License at http://www.mozilla.org/MPL/
--- data/script.js
+++ data/script.js
@@ -1,37 +1,59 @@
 /* ***** BEGIN LICENSE BLOCK *****
  *
- * Plurk2Plurk
+ * Firefox Plurk2Plurk extension v0.5
  *
  * This add-on is licensed under Mozilla Public License Version 1.1
  * You may obtain a copy of the License at http://www.mozilla.org/MPL/
  *
  * ***** END LICENSE BLOCK ***** */
 
-console.log("Plurk2Plurk: Script Active");
-
 $('iframe').each(function(i){
     var fbEyeFrame = $(this);
     var fbUrl = decodeURIComponent(fbEyeFrame.attr('src'));
     var urltest = /^https?:\/\/www\.facebook\.com\/plugins\/like\.php\?.*$/;
-    //console.log(fbUrl + ' ' + urltest.test(fbUrl));
     
-    if (urltest.test(fbUrl)){    
+    // Check for facebook Like button
+    if (urltest.test(fbUrl)){
+    
         fbUrl = getUrlPara(fbUrl, 'href');
-        //console.log('iframe[' + i + '] #' + fbEyeFrame.attr('id') + ' url: ' + fbUrl + ' html: ' + urlTitle);        
-
-        var shareContent = fbUrl + " (" + $(document).attr('title') + ")";
-        var plurkBtn = $("<img alt='' src='http://statics.plurk.com/23d30a60b81915a637d1c3d2cd966a59.png ' width='65' height='20' border='0' /></a>").click(function() {
-            window.open("http://www.plurk.com/?qualifier=shares&status=" + encodeURIComponent(shareContent));
-        });
         
-        plurkBtn.insertBefore(document);
+        var shareContent = fbUrl + " (" + $(document).attr('title') + ")";
+        var plurkBtn = $("<div style='display: none; position: absolute; top: 0px; left: 0px;' class='p2plurk_div'><a title='Share to Plurk' href='#' class='p2plurk' style='float: left;'><img alt='' src='http://statics.plurk.com/23d30a60b81915a637d1c3d2cd966a59.png ' width='65' height='20' border='0' /></a></div>")
+            .css('left', fbEyeFrame.offset().left).css('top', fbEyeFrame.offset().top + 22)
+            .click(function() {
+                window.open("http://www.plurk.com/?qualifier=shares&status=" + encodeURIComponent(shareContent));
+            })
+            .hover(
+                function(){ $('body').data('pHoverStat', true); },
+                function(){
+                    $('body').data('pHoverStat', false);
+                    setTimeout(hideBtn, 500);
+                }
+            )
+            .appendTo('body');        // Append Plurk button to body
+            
+        fbEyeFrame.hover(
+            function() {
+                $('body').data('pHoverStat', true);
+                plurkBtn.show('fast');
+            },
+            function() {
+                $('body').data('pHoverStat', false);
+                setTimeout(hideBtn, 500);
+            }
+        );
     };
-
 });
 
+// Hide Plurk button on timeout after Timeout
+function hideBtn() {
+    if ($('body').data('pHoverStat') == false){
+        $('.p2plurk_div').hide('fast'); 
+    };
+};
 
-function getUrlPara(str, name)
-{
+// Get Url parameter
+function getUrlPara(str, name){
   name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
   var regexS = "[\\?&]"+name+"=([^&#]*)";
   var regex = new RegExp( regexS );
