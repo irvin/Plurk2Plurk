5e83ae7cb8f7a00f0d2e3b60f19b89340a442cfa
Irvin <irvinfly@gmail.com>
2011-12-12 18:12:02 +0800

    Support for XFBML
    Signed-off-by: Irvin <irvinfly@gmail.com>

--- data/script.js
+++ data/script.js
@@ -8,43 +8,57 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-$('iframe').each(function(i){
-    var fbEyeFrame = $(this);
-    var fbUrl = decodeURIComponent(fbEyeFrame.attr('src'));
-    var urltest = /^https?:\/\/www\.facebook\.com\/plugins\/like\.php\?.*$/;
-    
-    // Check for facebook Like button
-    if (urltest.test(fbUrl)){
-    
-        fbUrl = getUrlPara(fbUrl, 'href');
-        
-        var shareContent = fbUrl + " (" + $(document).attr('title') + ")";
-        var plurkBtn = $("<div style='display: none; position: absolute; top: 0px; left: 0px;' class='p2plurk_div'><a title='Share to Plurk' href='#' class='p2plurk' style='float: left;'><img alt='' src='http://statics.plurk.com/23d30a60b81915a637d1c3d2cd966a59.png ' width='65' height='20' border='0' /></a></div>")
-            .css('left', fbEyeFrame.offset().left).css('top', fbEyeFrame.offset().top + 22)
-            .click(function() {
-                window.open("http://www.plurk.com/?qualifier=shares&status=" + encodeURIComponent(shareContent));
-            })
-            .hover(
-                function(){ $('body').data('pHoverStat', true); },
-                function(){
-                    $('body').data('pHoverStat', false);
-                    setTimeout(hideBtn, 500);
-                }
-            )
-            .appendTo('body');        // Append Plurk button to body
-            
-        fbEyeFrame.hover(
-            function() {
-                $('body').data('pHoverStat', true);
-                plurkBtn.show('fast');
-            },
-            function() {
+console.log('p2plurk activate!');
+
+var fbXxxxl = $('fb\\:like');
+var fbTitle = $(document).attr('title');
+
+if (fbXxxxl.length) {    //Find FBXML    
+    fbXxxxl.each(function(i){
+        var fbEyeFrame = $(this);
+        var fbUrl = fbEyeFrame.attr('href') || window.location;
+        addBtn(fbUrl, fbTitle, fbEyeFrame);
+    });
+} 
+else {    //Check fb iframe
+    $('iframe').each(function(i){
+        var fbEyeFrame = $(this);
+        var fbUrl = decodeURIComponent(fbEyeFrame.attr('src'));
+        var urltest = /^https?:\/\/www\.facebook\.com\/plugins\/like\.php\?.*$/;
+
+        // Check for facebook Like button
+        if (urltest.test(fbUrl)){        
+            fbUrl = getUrlPara(fbUrl, 'href');
+            addBtn(fbUrl, fbTitle, fbEyeFrame);
+        };
+    });
+};
+
+
+function addBtn(fbUrl, title, target) {
+    var shareContent = fbUrl + " (" + title + ")";
+    var plurkBtn = $("<div style='display: none; position: absolute; top: 0px; left: 0px;' class='p2plurk_div'><a title='Share to Plurk' href='http://www.plurk.com/?qualifier=shares&status=" + encodeURIComponent(shareContent) + "' class='p2plurk' target='_blank'><img alt='' src='http://statics.plurk.com/23d30a60b81915a637d1c3d2cd966a59.png ' width='65' height='20' border='0' /></a></div>")
+        .css('left', target.offset().left).css('top', target.offset().top + 22)
+        .hover(
+            function(){ $('body').data('pHoverStat', true); },
+            function(){
                 $('body').data('pHoverStat', false);
                 setTimeout(hideBtn, 500);
             }
-        );
-    };
-});
+        )
+        .appendTo('body');        // Append Plurk button to body
+        
+    target.hover(
+        function() {
+            $('body').data('pHoverStat', true);
+            plurkBtn.show('fast');
+        },
+        function() {
+            $('body').data('pHoverStat', false);
+            setTimeout(hideBtn, 500);
+        }
+    );
+}
 
 
 // Hide Plurk button on timeout after Timeout
