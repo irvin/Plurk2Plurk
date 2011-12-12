3df7046e70990f0013b388be15ded8bb885eae29
Irvin <irvinfly@gmail.com>
2011-12-12 13:18:21 +0800

    finished insert button, work ok, but make fb button position error
    Signed-off-by: Irvin <irvinfly@gmail.com>

--- data/script.js
+++ data/script.js
@@ -7,30 +7,27 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-console.log(" ");
 console.log("Plurk2Plurk: Script Active");
 
-
-var fbDetective = $('iframe');
-fbDetective.css('border','2px solid yellow');
-
-fbDetective.each(function(i){
-
-    var fbiframe = $(this);
-    var fbUrl = decodeURIComponent(fbiframe.prop('src'));
-    console.log('iframe [' + i + '] url: ' + fbUrl);
-        
+$('iframe').each(function(i){
+    var fbEyeFrame = $(this);
+    var fbUrl = decodeURIComponent(fbEyeFrame.attr('src'));
     var urltest = /^https?:\/\/www\.facebook\.com\/plugins\/like\.php\?.*$/;
-    if (urltest.test(fbUrl)){
-        fbiframe.css('background-color','red');        
+    //console.log(fbUrl + ' ' + urltest.test(fbUrl));
+    
+    if (urltest.test(fbUrl)){    
         fbUrl = getUrlPara(fbUrl, 'href');
+        //console.log('iframe[' + i + '] #' + fbEyeFrame.attr('id') + ' url: ' + fbUrl + ' html: ' + urlTitle);        
+
+        var shareContent = fbUrl + " (" + $(document).attr('title') + ")";
+        var plurkBtn = $("<img alt='' src='http://statics.plurk.com/23d30a60b81915a637d1c3d2cd966a59.png ' width='65' height='20' border='0' /></a>").click(function() {
+            window.open("http://www.plurk.com/?qualifier=shares&status=" + encodeURIComponent(shareContent));
+        });
         
-        var urlTitle = fbiframe.contents().find('.connect_widget_text').find('b').text();
-        console.log('Found fb iframe, target Url: ' + fbUrl + ' Title: ' + urlTitle);
+        plurkBtn.insertBefore(document);
     };
 
 });
-
 
 
 function getUrlPara(str, name)
@@ -44,73 +41,3 @@ function getUrlPara(str, name)
   else
     return results[1];
 }
-
-//http://www.facebook.com/plugins/like.php
-
-//var fbDetective = if.contents().find('html#facebook');
-//if (fbDetective.text('background-color','red')) {
-//    fbDetective.css();
-//}
-
-//$("#mainiframe").contents().find("someID").html()
-
-
-//fbDetective.css('background-color','red').parent().css('background-color','green');
-
-//var fbDetective = $('html#facebook').parentsUntil('iframe');
-//if (fbDetective.text()) {
-//    console.log(fbDetective.html());
-//    fbDetective.css('background-color','red');
-//};
-/*
-if ( fbDetective.text() && !fbDetective.hasClass('P2PlurkInjected') ) {
-    fbDetective.addClass('P2PlurkInjected').css('background-color','red');
-    var fbUri = decodeURI(fbDetective.attr('src'));
-    console.log(fbUri);   
-};
-*/
-
-//var strTitle = $('h1').text();
-//var btnViewInItunes = $("div#left-stack a.view-in-itunes:first, div#left-stack a.view-in-appstore:first");
-//if (btnViewInItunes.height() > 30) btnViewInItunes.height(23);  //Fix Mac App Store page button position
-//
-//var button = $("<div>Find At AppShopper</div>")
-//    .css({
-//        'font-family': '"Myriad Pro", Arial, Helvetica, sans-serif',
-//        'font-size': '11px',
-//        'color': '#FFF',
-//        'margin': '-5px 0 10px',
-//        'padding': '1px 10px',
-//        'background': '-moz-linear-gradient( top, #57B8EE 0%, #2681C8)',
-//        'border-radius': '20px',
-//        'text-shadow': '0px -1px 1px rgba(0, 0, 0, 0.25)',
-//        'cursor': 'pointer',
-//        'box-shadow': '0 1px 1px rgba(0, 0, 0, 0.5)',
-//        'width': '-moz-fit-content'
-//    })
-//    .insertAfter(btnViewInItunes)
-//    .click(function(){
-//        window.open("http://appshopper.com/search/?search=" + strTitle)    
-//    });
-    
-
-//<a title=”Share to Plurk” href=”javascript:
-//
-//void(
-//    window
-//        .open(‘http://www.plurk.com/?qualifier=shares&status=‘
-//            .concat(
-//                encodeURIComponent(window.location.href)
-//            )
-//            .concat(‘ ‘)
-//            .concat(‘(‘)
-//            .concat(
-//                encodeURIComponent(document.title)
-//            )
-//            .concat(‘)‘)
-//        )
-//    );
-//
-//”>
-//<img title=”share” src=”http://statics.plurk.com/bda225d234426cccca300c551f60438e.png” width=”97” height=”20” align=”absmiddle” border=”0” />
-//</a>
